3774c375f12e624d93bb466a231f5960526036cf
rokasb <rokasb@stud.ntnu.no>
2021-04-15 12:25:01 +0200

    feat(https): create makefile

---
+++ backend/src/main/java/idatt2106/group3/backend/Security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package idatt2106.group3.backend.Security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .requiresChannel()
+            .anyRequest()
+            .requiresSecure();
+    }
+}
---
+++ backend/src/test/java/idatt2106/group3/backend/HttpsTest.java
@@ -0,0 +1,36 @@
+package idatt2106.group3.backend;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Collections;
+
+import javax.net.ssl.SSLContext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest
+class HttpsTest {
+
+    @Test
+    public void whenGETanHTTPSResource_thenCorrectResponse() throws Exception {
+        SSLContext sslContext = new SSLContextBuilder()
+        .loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray())
+        .build();
+      SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
+      HttpClient httpClient = HttpClients.custom()
+        .setSSLSocketFactory(socketFactory)
+        .build();
+      HttpComponentsClientHttpRequestFactory factory = 
+        new HttpComponentsClientHttpRequestFactory(httpClient);
+      return new RestTemplate(factory);        
+        
+        ResponseEntity<String> response = restTemplate().getForEntity(WELCOME_URL, String.class, Collections.emptyMap());
+    
+        assertEquals("<h1>Welcome to Secured Site</h1>", response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+}

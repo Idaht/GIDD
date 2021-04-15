bbbbc3ce6d5270b308125c0ddd4d35fa67b3f6e1
Linda Larsen <lindalar@stud.ntnu.no>
2021-04-15 11:16:05 +0200

    La til global CSS i App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,12 +7,22 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Mulish&display=swap');
+
+$font-stack: 'Mulish', sans-serif;
+$primary-color: #282828;
+$secondary-color: #EA4B4B;
+$padding: 0.6rem 1rem 0.6rem 1rem;
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Mulish', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  font-size: 16px;
+  letter-spacing: 0.04rem;
+  display: grid;
 }
 
 #nav {
@@ -20,11 +30,55 @@
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $secondary-color;
 
-    &.router-link-exact-active {
+    &.router-link-exact {
       color: #42b983;
     }
   }
+}
+
+h1 {
+  font-size: 1.44rem;
+}
+
+h2 {
+  font-size: 1.2rem;
+}
+
+p {
+  line-height: 150%;
+}
+
+button {
+  background: #EA4B4B;
+  border: 0px solid #000000;
+  box-sizing: border-box;
+  border-radius: 40px;
+  padding: $padding;
+  font-variant: small-caps;
+  font-family: 'mulish';
+  font-weight:600;
+  color: white;
+  font-size: 0.833rem;
+}
+
+input {
+  background: #FFFFFF;
+  border: 1px solid #9F9F9F;
+  box-sizing: border-box;
+  border-radius: 22px;
+  padding: $padding;
+  font-weight: normal;
+  font-family: 'mulish';
+  font-size: 1rem;
+  color: $primary-color !important;
+  margin: auto;
+  display: inline;
+}
+
+a:link {
+  color: $primary-color;
+  
 }
 </style>

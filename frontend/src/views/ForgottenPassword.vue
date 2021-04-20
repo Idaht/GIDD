cf50ff32160f3130f4bedbf035af00ad1057731b
Linda Katrine Larsen <lindalar@stud.ntnu.no>
2021-04-20 14:38:13 +0000

    feat(forgotten-password-ui): Modified CSS and HTML structure

--- frontend/src/views/ForgottenPassword.vue
+++ frontend/src/views/ForgottenPassword.vue
@@ -1,18 +1,20 @@
 <template>
-  <div>
+  <div id="forgotten-password">
     <!--Add TheHeader component -->
-    <h1>Tilbakestill passord</h1>
+    <h1>Tilbakestill</h1>
+    <h1>passord</h1>
     <p>
       Vennligst skriv inn e-postadressen du registrerte deg med, så vil du motta
       en e-post med informasjon om gjennoppretting av passord
     </p>
     <input v-model="email" type="email" placeholder="E-post" id="email" />
-    <button
-      @click="sendMailForgotPassword"
+    <div class="send-btn">
+    <button @click="sendMailForgotPassword"
       alt="Knapp for å sende e-post når passord er glemt"
     >
       Send
     </button>
+    </div>
     <p id="feedback" v-if="emailIsNotEmpty">{{ makeEmailFeedback }}</p>
   </div>
 </template>
@@ -61,3 +63,33 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped lang="scss">
+#forgotten-password {
+  width: 340px;
+  margin: 0 auto;
+  box-sizing: border-box;
+  padding-top: 90px;
+}
+
+h1 {
+  line-height: 0.5em;
+}
+
+p {
+  margin: 25px;
+  text-align: left;
+}
+
+button {
+  margin: 1.5em 0 1em 0;
+  align-content: center;
+  text-transform: uppercase;
+  font-weight: 600;
+  letter-spacing: 1px;
+}
+
+input {
+  margin: 1em 0 0 0;
+}
+</style>

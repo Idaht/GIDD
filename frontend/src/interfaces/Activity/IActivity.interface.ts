859e6554cb92d4c11fb14968bc999846178cf9aa
Martin Slind Hagen <martsha@stud.ntnu.no>
2021-04-30 07:13:49 +0000

    feat(activity-display): Displaying name and trusted for activity organiser, added ability to register absence for an activity

--- frontend/src/interfaces/Activity/IActivity.interface.ts
+++ frontend/src/interfaces/Activity/IActivity.interface.ts
@@ -3,4 +3,6 @@ import MakeActivity from "./MakeActivity.interface";
 export default interface IActivity extends MakeActivity {
   activityId: number;
   organizerId: number;
+  orgarganizerForename?: string;
+  organizerSurname?: string;
 }

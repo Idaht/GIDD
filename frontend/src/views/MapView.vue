279514d09ff262f1dba127609d6d311f49b4094e
Linda Katrine Larsen <lindalar@stud.ntnu.no>
2021-04-30 08:20:38 +0000

    feat(activity-feed-ui): added styling to list-view and map-view

--- frontend/src/views/MapView.vue
+++ frontend/src/views/MapView.vue
@@ -1,57 +1,87 @@
 <template>
-  <div id="map-view">
-    <div>
-      <div class="header" id="upper-header">
-        <div id="header-title">
-          <h2>Finn aktiviteter</h2>
+  <div>
+    <div id="map-view">
+      <div>
+        <div class="header" id="upper-header">
+          <div id="header-title">
+            <h2>Finn aktiviteter</h2>
+          </div>
+          <div id="view-container">
+            <button
+              @click="feedViewClicked"
+              id="view-list"
+              class="icon"
+            ></button>
+            <button id="view-map" class="icon"></button>
+          </div>
         </div>
-        <div id="view-container">
-          <button @click="feedViewClicked" id="view-list" class="icon"></button>
-          <button id="view-map" class="icon"></button>
-        </div>
-      </div>
 
-      <div class="header" id="lower-header">
-        <div id="filter-boxes" class="lower-header-item">
+        <div class="header" id="lower-header">
+          <div id="test">
+            <div>
+              <select
+                class="dropdown lower-header-item"
+                v-model="sortingType"
+                @change="sortClicked"
+              >
+                <option selected hidden>Sortering</option>
+                <option value="DATE">Startdato</option>
+                <option value="DISTANCE">Avstand</option>
+                <option value="PARTICIPANT_AMOUNT">Antall deltakere</option>
+                <option value="REMOVE_SORT">Nullstill</option>
+              </select>
+            </div>
+
+            <div>
+              <input
+                type="text"
+                id="search"
+                v-model="searchQuery"
+                @change="sortClicked"
+                class="lower-header-item"
+                placeholder="Søk"
+              />
+            </div>
+          </div>
+
+          <div id="filter-boxes" class="lower-header-item">
             <div class="checkbox-label">
               <label for="easy">Lett</label>
               <input
                 type="checkbox"
                 v-model="easyCheckbox"
-                @change="filterClicked"
+                @change="sortClicked"
               />
             </div>
             <div class="checkbox-label">
-              <label for="easy">Medium</label>
+              <label for="easy">Middels</label>
               <input
                 type="checkbox"
                 v-model="mediumCheckbox"
-                @change="filterClicked"
+                @change="sortClicked"
               />
             </div>
             <div class="checkbox-label">
-              <label for="easy">Hardt</label>
+              <label for="easy">Krevende</label>
               <input
                 type="checkbox"
                 v-model="hardCheckbox"
-                @change="filterClicked"
+                @change="sortClicked"
               />
             </div>
           </div>
-          <input
-            type="text"
-            id="search"
-            v-model="searchQuery"
-            @change="filterClicked"
-            class="lower-header-item"
-            placeholder="Søk"
-          />
+        </div>
+      </div>
+      <div id="activities">
+        <Map
+          id="map"
+          :center="{ lat: 63.43049, lng: 10.39506 }"
+          :activityData="activities"
+        >
+        </Map>
       </div>
     </div>
-
-    <div id="activities">
-      <Map id="map" :activityData="activities"></Map>
-    </div>
+    <div id="add-activity" @click="makeActivity">+</div>
   </div>
 </template>
 
@@ -174,8 +204,11 @@ $padding: 0.6rem 1rem 0.6rem 1rem;
   @media only screen and (min-width: 600px) {
     width: 45%;
     margin: auto;
-    grid-template-columns: 1fr 1fr;
   }
+}
+
+h2 {
+  text-align: center;
 }
 
 .header {
@@ -189,9 +222,9 @@ $padding: 0.6rem 1rem 0.6rem 1rem;
 }
 
 #lower-header {
-  margin-bottom: 30px;
   display: grid;
   grid-template-columns: 1fr 3fr;
+  row-gap: 20px;
 }
 
 h2 {
@@ -211,7 +244,7 @@ h2 {
 }
 
 #view-map {
-  background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23C2C2C2' viewBox='0 0 24 24'%3E%3Cpath fill-rule='evenodd' d='M12 21c-1.5 0-7-6-7-11a7 7 0 1114 0c0 5-5.5 11-7 11zm0-8.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z'/%3E%3C/svg%3E");
+  background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23c2c2c2' viewBox='0 0 24 24'%3E%3Cpath fill-rule='evenodd' d='M12 21c-1.5 0-7-6-7-11a7 7 0 1114 0c0 5-5.5 11-7 11zm0-8.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z'/%3E%3C/svg%3E");
 }
 
 .icon {
@@ -224,12 +257,56 @@ h2 {
   background-color: unset;
 }
 
-#lower-header-sort,
-#lower-header-filter {
+.checkbox-label {
+  display: flex;
+  flex-direction: column;
+  width: 10%;
+  align-items: center;
+}
+
+#filter-boxes {
+  display: grid;
+  width: 100%;
+  grid-template-columns: 33% 33% 33%;
+  border-radius: 10px;
+  border-bottom: 2px #e1e1e1 solid;
+  justify-items: center;
+  justify-self: center;
+  padding: 5px 0 5px 0;
+  @media only screen and (min-width: 600px) {
+    width: 45%;
+    margin: auto;
+  }
+}
+
+#search {
+  display: block;
+}
+
+#test {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 20px;
+}
+
+#lower-header {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 1fr;
+  column-gap: 10px;
+  align-items: flex-end;
+}
+
+input {
+  width: 100%;
+}
+
+.lower-header-item {
   border-radius: 20px;
   font-size: 10px;
   letter-spacing: 1px;
-  text-transform: uppercase;
+  border-color: #9f9f9f;
+  color: #27282b;
   font-weight: 600 !important;
   padding: 5px 10px 5px 10px;
 }
@@ -249,16 +326,68 @@ option {
   line-height: 1rem;
 }
 
+#activity-button {
+  position: fixed;
+  bottom: 10px;
+  left: 50%;
+}
+
 select {
   background: url(data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA0Ljk1IDEwIj48ZGVmcz48c3R5bGU+LmNscy0xe2ZpbGw6I2ZmZjt9LmNscy0ye2ZpbGw6IzQ0NDt9PC9zdHlsZT48L2RlZnM+PHRpdGxlPmFycm93czwvdGl0bGU+PHJlY3QgY2xhc3M9ImNscy0xIiB3aWR0aD0iNC45NSIgaGVpZ2h0PSIxMCIvPjxwb2x5Z29uIGNsYXNzPSJjbHMtMiIgcG9pbnRzPSIxLjQxIDQuNjcgMi40OCAzLjE4IDMuNTQgNC42NyAxLjQxIDQuNjciLz48cG9seWdvbiBjbGFzcz0iY2xzLTIiIHBvaW50cz0iMy41NCA1LjMzIDIuNDggNi44MiAxLjQxIDUuMzMgMy41NCA1LjMzIi8+PC9zdmc+)
     no-repeat 93% 50%;
   font-family: "Mulish", sans-serif;
   font-weight: 600;
+  width: 100%;
   letter-spacing: 1px;
   -moz-appearance: none;
   -webkit-appearance: none;
   -webkit-border-radius: 0px;
   appearance: none;
   outline-width: 0;
+  border-color: #bbbbbb;
+}
+
+#activities {
+  margin-top: 35px;
+  display: flex;
+  flex-flow: wrap;
+  position: relative;
+}
+
+#activity {
+  padding: 10px 0px 10px 0px;
+  @media only screen and (min-width: 800px) {
+  }
+}
+
+li {
+  list-style-type: none;
+  padding-bottom: 20px;
+  @media only screen and (min-width: 800px) {
+    width: 50%;
+  }
+}
+
+#add-activity {
+  line-height: 38px;
+  border-radius: 40px;
+  position: fixed;
+  font-size: 1.3rem;
+  bottom: 20px;
+  height: 40px;
+  width: 40px;
+  left: 50%;
+  margin-left: -20px;
+  color: #ffffff;
+  background-color: $secondary-color;
+  @media only screen and (min-width: 600px) {
+    left: 76%;
+    bottom: 40px;
+  }
+}
+
+#add-activity:hover {
+  cursor: pointer;
+  background-color: #ff6666;
 }
 </style>
